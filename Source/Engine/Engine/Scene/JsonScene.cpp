a3b041ac23dbf7b2e78aa45b410b54f6e6f41d86
Marcus Holst <macke.holst@gmail.com>
2016-09-27 20:19:50 +0200

    Fixed a compile error

--- Source/Engine/Engine/Scene/JsonScene.cpp
+++ Source/Engine/Engine/Scene/JsonScene.cpp
@@ -13,6 +13,7 @@
 
 JsonScene::JsonScene(const char* aFilePath) : Scene(aFilePath)
 {
+	CreateFactories();
 }
 
 
@@ -35,17 +36,15 @@ void JsonScene::Render()
 
 void JsonScene::CreateFactories()
 {
-	AddComponentFactory<ModelComponent>();
-	AddComponentFactory<LightComponent>();
-
 	std::shared_ptr<GameObject> enemy = std::make_shared<GameObject>();
 
-	std::shared_ptr<ModelComponent> prettyModel = GetComponentFactory<ModelComponent>()->CreateComponent();
+	//GetComponentFactory<ModelComponent>()->CreateComponent();
+
+	SharedPtrComponent<ModelComponent> prettyModel(GetComponentFactory<ModelComponent>()->CreateComponent());
 	std::shared_ptr<AssimpModel> model = std::make_shared<AssimpModel>(myEffect, "models/Modelviewer_Exempelmodell/K11_1415.fbx");
 	prettyModel->SetModel(std::make_shared<ModelInstance>(model));
 
-
 	GetCamera().SetPosition(model->GetBoundingBox().GetCenter() + Vector3f(0.f, 0.f, -model->GetBoundingBox().GetSize().z * 1.5f));
-
-	enemy->AddComponent<std::shared_ptr<ModelComponent>>(prettyModel);
+	
+	enemy->AddComponent<ModelComponent>(prettyModel);
 }
