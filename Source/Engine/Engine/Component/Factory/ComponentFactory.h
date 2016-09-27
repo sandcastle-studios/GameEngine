8c89a538ed40621d429739bf398b94bea19fadb3
Andreas Lasses <andreas.lasses@gmail.com>
2016-09-27 14:51:58 +0200

    components are working very well with sharedptrs (tm) by Ylva and Andreas

--- Source/Engine/Engine/Component/Factory/ComponentFactory.h
+++ Source/Engine/Engine/Component/Factory/ComponentFactory.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <Utilities/Container/GrowingArray.h>
 #include "Engine/Component/Factory/BaseComponentFactory.h"
+#include <Utilities/Container/SharedPtrGA.h>
 
 template <typename TComponentType>
 class ComponentFactory : public BaseComponentFactory
@@ -9,34 +10,47 @@ public:
 	ComponentFactory() {};
 	~ComponentFactory() {};
 
-	std::shared_ptr<TComponentType> CreateComponent()
-	{
-		myComponents.Add(std::make_shared<TComponentType>());
-		return myComponents.GetLast();
-	}
+	SharedPtrGA<TComponentType> ComponentFactory<TComponentType>::CreateComponent();
+	SharedPtrGA<TComponentType> ComponentFactory<TComponentType>::GetComponent(unsigned short aIndex)const;
 
 	virtual void Update(const Time & aDeltaTime) override;
 
 	virtual void Render() override;
 
 private:
-	GrowingArray<std::shared_ptr<TComponentType>, size_t> myComponents;
+	GrowingArray<TComponentType> myComponents;
 };
+
+template <typename TComponentType>
+SharedPtrGA<TComponentType> ComponentFactory<TComponentType>::GetComponent(unsigned short aIndex)const
+{
+	//SharedPtrGA<TComponentType> component(myComponents, myComponents.GetLast());
+	return SharedPtrGA<TComponentType> (myComponents, myComponents.GetLast());
+
+	//return SharedPtrGA<TComponentType>(myComponents, myComponents[aIndex]);
+}
+
+template <typename TComponentType>
+SharedPtrGA<TComponentType> ComponentFactory<TComponentType>::CreateComponent()
+{
+	myComponents.Add(TComponentType());
+	return SharedPtrGA<TComponentType> (myComponents, myComponents.GetLast());
+}
 
 template <typename TComponentType>
 inline void ComponentFactory<TComponentType>::Update(const Time & aDeltaTime)
 {
-	for (size_t iComponent = 0; iComponent < myComponents.Size(); ++iComponent)
+	for (unsigned short iComponent = 0; iComponent < myComponents.Size(); ++iComponent)
 	{
-		myComponents[iComponent]->Update(aDeltaTime);
+		myComponents[iComponent].Update(aDeltaTime);
 	}
 }
 
 template <typename TComponentType>
 inline void ComponentFactory<TComponentType>::Render()
 {
-	for (size_t iComponent = 0; iComponent < myComponents.Size(); ++iComponent)
+	for (unsigned short iComponent = 0; iComponent < myComponents.Size(); ++iComponent)
 	{
-		myComponents[iComponent]->Render();
+		myComponents[iComponent].Render();
 	}
 }
