bfb28567d697ee1ba9fa2443499668689f3f6fc7
Ylf skola <ylva.werner.sp15@thegameassembly.com>
2016-09-29 14:09:24 +0200

    SharedPtr comparing operators!

---
+++ Source/Game/Game/SprintReviewScene.cpp
@@ -0,0 +1,54 @@
+#include "stdafx.h"
+#include "SprintReviewScene.h"
+#include <Engine\Component\Factory\ComponentFactory.h>
+#include <Engine/Model/ModelInstance.h>
+#include <Engine\Component\ModelComponent.h>
+#include <Engine\Component\LightComponent.h>
+#include <Engine\GameObject\GameObject.h>
+#include <Engine\Model\AssimpModel.h>
+#include <Engine/Effect/StandardEffect.h>
+#include <Engine\Camera/Camera.h>
+
+SprintReviewScene::SprintReviewScene()
+{
+	CreateFactories();
+}
+
+SprintReviewScene::~SprintReviewScene()
+{
+}
+
+void SprintReviewScene::Update(const Time & aDeltaTime)
+{
+	Scene::Update(aDeltaTime);
+}
+
+void SprintReviewScene::Render()
+{
+	Scene::Render();
+}
+
+void SprintReviewScene::CreateFactories()
+{
+	PreCreateComponentFactory<ModelComponent>();
+	PreCreateComponentFactory<LightComponent>();
+
+	std::shared_ptr<GameObject> enemy = std::make_shared<GameObject>();
+
+	//GetComponentFactory<ModelComponent>()->CreateComponent();
+
+	SharedPtrComponent<ModelComponent> prettyModel(GetComponentFactory<ModelComponent>()->CreateComponent());
+	std::shared_ptr<AssimpModel> model = std::make_shared<AssimpModel>(myEffect, "models/Modelviewer_Exempelmodell/K11_1415.fbx");
+	prettyModel->SetModel(std::make_shared<ModelInstance>(model));
+
+	SharedPtrComponent<ModelComponent> moarModel(GetComponentFactory<ModelComponent>()->CreateComponent());
+	std::shared_ptr<AssimpModel> actualModel = std::make_shared<AssimpModel>(myEffect, "models/Stefan/testSpheres.fbx");
+	moarModel->SetModel(std::make_shared<ModelInstance>(actualModel));
+
+	enemy->AddComponent<ModelComponent>(prettyModel);
+	enemy->AddComponent<ModelComponent>(moarModel);
+
+	myObjects.Add(enemy);
+
+	GetCamera().SetPosition(model->GetBoundingBox().GetCenter() + Vector3f(0.f, 0.f, -model->GetBoundingBox().GetSize().z * 1.5f));
+}
---
+++ Source/Game/Game/SprintReviewScene.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <Engine/Scene/Scene.h>
+
+class Time;
+
+class SprintReviewScene : public Scene
+{
+public:
+	SprintReviewScene();
+	~SprintReviewScene();
+
+	virtual void Update(const Time & aDeltaTime) override;
+	virtual void Render() override;
+
+private:
+	void CreateFactories();
+
+
+};
+
